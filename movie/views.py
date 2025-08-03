19d9898b5c0e4721acb862f27e4799f965765003
julitZr <julianosorioa01@gmail.com>
2025-08-03 18:17:30 -0500

    visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -14,7 +14,7 @@ def home(request):
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies= Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+    return render(request, 'home.html' ,{'name': 'Julian Osorio', 'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
    #return render(request, 'about.html')
